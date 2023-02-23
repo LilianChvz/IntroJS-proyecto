3bd92e03904df91f654a25127dba8600a17cb83c
lilianchavez <lauralilian2002@gmail.com>
2023-02-22 21:30:22 -0600

    estructura de cajero

---
+++ deHome.js
@@ -0,0 +1,39 @@
+// CONSULTA DE SALDO
+function c_saldo(form) {
+    location="Saldo.html"
+}
+
+function regreso(form) {
+    location = "Home.html"
+}
+
+
+
+// DEPOSITO
+function deposito(form) {
+    location = "Deposito.html"
+}
+
+function regreso(form) {
+    location = "Home.html"
+}
+
+function depositoV(form) {
+    document.querySelector("#CS").innerHTML = cant + CS;
+    
+}
+
+
+
+
+
+
+// RETIRO
+function retiro(form) {
+    location = "Retiro.html"
+}
+
+function regreso(form) {
+    location = "Home.html"
+}
+
---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+function login(form){
+    if (form.u.value == "Mali") {
+        if (form.c.value == "1231") {
+            location= "Home.html"
+        } else {
+            alert("Contraseña incorrecta");
+        }
+        
+    } else {
+        alert("Error de usuario");
+    }
+}
+
+
+
+
+
+// function login1(form){
+//     if (form.u.value == "Gera") {
+//         if (form.c.value == "1232") {
+//             location= "Home.html"
+//         } else {
+//             alert("Contraseña incorrecta");
+//         }
+        
+//     } else {
+//         alert("Error de usuario");
+//     }
+// }
+
+// function login2(form){
+//     if (form.u.value == "Maui") {
+//         if (form.c.value == "1233") {
+//             location= "Home.html"
+//         } else {
+//             alert("Contraseña incorrecta");
+//         }
+        
+//     } else {
+//         alert("Error de usuario");
+//     }
+// }
